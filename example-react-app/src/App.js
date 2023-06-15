6156136724b3aac0ad6fa659385447d4e11ab464
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-06-15 00:58:10 -0700

    Custom.css via custom.scss added.

--- example-react-app/src/App.js
+++ example-react-app/src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div>
       <h1>Hello World from React!</h1>
+      <button className="custom-button">Custom Button</button>
+      <button className="btn btn-primary">Custom Button</button>
+      <button className="btn btn-coral">Custom Button</button>
     </div>
   );
 }
